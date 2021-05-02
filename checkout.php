a8fd78a0ae0627ca836a4098f4865fb70177645b
colin colraine <colinco83@hotmail.co.uk>
2021-05-02 18:44:10 +0100

    heroku 89

--- checkout.php
+++ checkout.php
@@ -3,31 +3,11 @@
          $subject = "This is subject";
          
          $message = "<b>This is HTML message.</b>";
-         $message .= "<h1>This is headline.</h1>";
          
-         $header = "From:ann@ogilviejerome.com \r\n";
-         $header .= "Cc:colinco83@hotmail.co.uk \r\n";
-         $header .= "MIME-Version: 1.0\r\n";
-         $header .= "Content-type: text/html\r\n";
+         $headers = "From:ann@ogilviejerome.com \r\n";
+         $headers .= "Cc:colinco83@hotmail.co.uk \r\n";
          
-         $retval = mail ($to,$subject,$message,$header);
+         mail($to,$subject,$message,$headers);
          
-         if( $retval == true ) {
-            echo "Message sent successfully...";
-         }else {
-            echo "Message could not be sent...";
-         }
+         header('Location: thank-you.html');
       ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Sending HTML email using PHP</title>
-</head>
-<body>
-<p>Hello World</p>
-</body>
-</html>
