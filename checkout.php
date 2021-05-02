d5d7069b6f636fec24c3f9cbfa092fa91058da8e
colin colraine <colinco83@hotmail.co.uk>
2021-05-02 14:29:58 +0100

    git 81

---
+++ checkout.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sending HTML email using PHP</title>
+</head>
+<body>
+<?php
+         $to = "amdcolraine777@gmail.com";
+         $subject = "This is subject";
+         
+         $message = "<b>This is HTML message.</b>";
+         $message .= "<h1>This is headline.</h1>";
+         
+         $header = "From:ann@ogilviejerome.com \r\n";
+         $header .= "Cc:colinco83@hotmail.co.uk \r\n";
+         $header .= "MIME-Version: 1.0\r\n";
+         $header .= "Content-type: text/html\r\n";
+         
+         $retval = mail ($to,$subject,$message,$header);
+         
+         if( $retval == true ) {
+            echo "Message sent successfully...";
+         }else {
+            echo "Message could not be sent...";
+         }
+      ?>
+</body>
+</html>
