c023836a58407eb8d55c160841e140af41d32af1
colin colraine <colinco83@hotmail.co.uk>
2021-05-03 11:38:25 +0100

    git 92

--- checkout.php
+++ checkout.php
@@ -1,19 +1,26 @@
 <?php
-         $to = "amdcolraine777@gmail.com";
-         $subject = "This is subject";
+         // The global $_POST variable allows you to access the data sent with the POST method by name
+         // To access the data sent with the GET method, you can use $_GET
+         $say = htmlspecialchars($_POST['say']);
+         $to  = htmlspecialchars($_POST['to']);
+       
+         echo  $say, ' ', $to;
          
-         $message = "<b>This is HTML message.</b>";
+         //$to = "amdcolraine777@gmail.com";
+         //$subject = "This is subject";
          
-         //$headers = "From:ann@ogilviejerome.com \r\n";
-         //$headers .= "Reply-To:amdcolraine777@gmail.com \r\n";
+         //$message = "<b>This is HTML message.</b>";
          
-         $headers .= "MIME-Version: 1.0\r\n";
-         $headers .= "Content-type: text/html\r\n";
-         $headers .= 'From: ann@ogilviejerome.com' . "\r\n" .
-         'Reply-To: amdcolraine777@gmail.com' . "\r\n" .
-         'X-Mailer: PHP/' . phpversion();
+         ////$headers = "From:ann@ogilviejerome.com \r\n";
+         ////$headers .= "Reply-To:amdcolraine777@gmail.com \r\n";
+         
+         //$headers .= "MIME-Version: 1.0\r\n";
+         //$headers .= "Content-type: text/html\r\n";
+         //$headers .= 'From: ann@ogilviejerome.com' . "\r\n" .
+         //'Reply-To: amdcolraine777@gmail.com' . "\r\n" .
+         //'X-Mailer: PHP/' . phpversion();
 
-         mail($to,$subject,$message,$headers);
+         //mail($to,$subject,$message,$headers);
          
-         header('Location: thank-you.html');
+         //header('Location: thank-you.html');
       ?>
