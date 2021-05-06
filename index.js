aaa8af25afd938696484bc1a9fa711ba0ee59615
colin colraine <colinco83@hotmail.co.uk>
2021-05-06 20:45:16 +0100

    git 107

--- index.js
+++ index.js
@@ -5,22 +5,15 @@ const session = require('express-session')
 //end new session stuff
 const mongoose = require('mongoose')
 
-//Nodemailer
-const cors = require("cors");
-const nodemailer = require("nodemailer");
-const multiparty = require("multiparty");
-require("dotenv").config();
-//Nodemailer
-
 const app = express()
 var path = require('path')
 const passport = require('./auth')
-
 
 const PORT = process.env.PORT || 4000
 
 const noteRoutes = require('./notes/routes')
 const userRoutes = require('./users/routes')
+const publicRoutes = require('./public/routes')
 
 const { username, password } = process.env
 const { EMAIL, PASS } = process.env
@@ -54,64 +47,8 @@ app.get('/', function(req, res) {
 
 app.use('/notes', noteRoutes)
 app.use('/users', userRoutes)
-
-//Nodemailer
-// cors
-app.use(cors({ origin: "*" }));
-
+app.use('/public', publicRoutes)
 //app.use("/public", express.static(process.cwd() + "/public")); //make public static
-
-//EMAIL = amdcolraine777@gmail.com,
-//PASS = Christopher/1933/AMDG
-
-const transporter = nodemailer.createTransport({
-  service: "smtp.live.com",
-  auth: {
-    user: process.env.EMAIL,
-    pass: process.env.PASS,
-  },
-});
-
-// verify connection configuration
-transporter.verify(function (error, success) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Server is ready to take our messages");
-  }
-});
-
-app.post("/send", (req, res) => {
-  let form = new multiparty.Form();
-  let data = {};
-  form.parse(req, function (err, fields) {
-    console.log(fields);
-    Object.keys(fields).forEach(function (property) {
-      data[property] = fields[property].toString();
-    });
-    console.log(data);
-    const mail = {
-      sender: `${data.sender} <${data.senderEmail}>`,
-      to: process.env.EMAIL, // receiver email,
-      subject: data.senderAge,
-      text: `${data.sender} <${data.senderEmail}> \n${data.message}`,
-    };
-    transporter.sendMail(mail, (err, data) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Something went wrong.");
-      } else {
-        res.status(200).send("Email successfully sent to recipient!");
-      }
-    });
-  });
-});
-
-//Index page (static HTML)
-//app.route("/").get(function (req, res) {
-//  res.sendFile(process.cwd() + "/public/contact.html");
-//});
-//Nodemailer
 
 app.listen(PORT, ()=>{
     console.log(`The app is running on http://localhost:${PORT}`)
---
+++ public/routes.js
@@ -0,0 +1,60 @@
+//Nodemailer
+const router = require('express').Router()
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const multiparty = require("multiparty");
+require("dotenv").config();
+
+app.use(cors({ origin: "*" }));
+
+//EMAIL = amdcolraine777@gmail.com,
+//PASS = Christopher/1933/AMDG
+
+const transporter = nodemailer.createTransport({
+  service: "smtp.live.com",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
+});
+
+// verify connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Server is ready to take our messages");
+  }
+});
+
+//Index page (static HTML)
+//app.route("/").get(function (req, res) {
+//  res.sendFile(process.cwd() + "/public/contact.html");
+//});
+//Nodemailer
+
+router.post("/send", (req, res) => {
+    let form = new multiparty.Form();
+    let data = {};
+    form.parse(req, function (err, fields) {
+      console.log(fields);
+      Object.keys(fields).forEach(function (property) {
+        data[property] = fields[property].toString();
+      });
+      console.log(data);
+      const mail = {
+        sender: `${data.sender} <${data.senderEmail}>`,
+        to: process.env.EMAIL, // receiver email,
+        subject: data.senderAge,
+        text: `${data.sender} <${data.senderEmail}> \n${data.message}`,
+      };
+      transporter.sendMail(mail, (err, data) => {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Something went wrong.");
+        } else {
+          res.status(200).send("Email successfully sent to recipient!");
+        }
+      });
+    });
+  });
