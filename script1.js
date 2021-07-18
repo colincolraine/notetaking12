58a63d45ae8b79b00e65515cbc02a6da3a7b8d9a
colin colraine <colinco83@hotmail.co.uk>
2021-07-18 12:58:34 +0100

    git 207

--- script1.js
+++ script1.js
@@ -1,7 +1,7 @@
 function myAjax() {
     $.ajax({
          type: "POST",
-         url: 'ajax.php',
+         url: '../ajax.php',
          data:{action:'call_this'},
          success:function(html) {
            alert(html);
