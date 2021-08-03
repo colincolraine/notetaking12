f9151ec0e66157cd783cb7fb7a225443879f55fc
colin colraine <colinco83@hotmail.co.uk>
2021-08-03 15:26:03 +0100

    git 211

--- users/routes.js
+++ users/routes.js
@@ -45,7 +45,7 @@ router.get('/dashboard', (req, res)=>{
     res.render('dashboard', {email: req.session.email})
 })
 
-router.post('/thank-you', (req, res)=>{
+router.get('/thank-you', (req, res)=>{
     //res.render(path.join(__dirname, 'dashboard.html'))
     //res.sendFile(path.join(process.cwd() + '/dashboard.html'))
     //res.render('dashboard')
